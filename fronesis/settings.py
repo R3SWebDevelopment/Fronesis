cd049c5eb455c40ab608f96822705e92d170fbd2
Luis Herrada <luisehk@gmail.com>
2017-02-21 18:05:12 -0600

    Add allauth basic settings

--- fronesis/settings.py
+++ fronesis/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+
+    'allauth',
+    'allauth.account',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +70,15 @@ TEMPLATES = [
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = (
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
 
 WSGI_APPLICATION = 'fronesis.wsgi.application'
 
@@ -112,6 +125,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+SITE_ID = 1
 
 
 # Static files (CSS, JavaScript, Images)
