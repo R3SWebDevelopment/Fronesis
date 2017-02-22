b49632321ab25c759fe09f1f2ae5a4e1c891bade
Luis Herrada <luisehk@gmail.com>
2017-02-22 00:36:20 -0600

    Remove drum frontend

--- fronesis/urls.py
+++ fronesis/urls.py
@@ -6,10 +6,4 @@ urlpatterns = [
     url(r'^admin/', include(admin_site.urls)),
     url(r'^rest-auth/', include('rest_auth.urls')),
     url(r'^docs/', get_swagger_view()),
-
-    url("^", include("drum.links.urls")),
-    url("^", include("mezzanine.urls")),
 ]
-
-handler404 = "mezzanine.core.views.page_not_found"
-handler500 = "mezzanine.core.views.server_error"
