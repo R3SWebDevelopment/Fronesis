e3af21115c4c2d0417225faeadbda6a86c2f2bbe
Luis Herrada <luisehk@gmail.com>
2017-02-22 23:54:33 -0600

    Add mixin for modelviewset, so people can only edit and delete their own posts

--- fronesis/philios/views.py
+++ fronesis/philios/views.py
@@ -1,14 +1,11 @@
-from rest_framework import viewsets
+from .mixins import OnlyAlterOwnObjectsViewSet
 from .serializers import LinkSerializer
 from drum.links.models import Link
 
 
-class LinkViewSet(viewsets.ModelViewSet):
+class LinkViewSet(OnlyAlterOwnObjectsViewSet):
     serializer_class = LinkSerializer
     queryset = Link.objects.all()
-
-    def get_queryset(self):
-        return self.queryset
 
     def perform_create(self, serializer):
         return serializer.save(
