00f59c6d8de0d219fa95d5db18846933ee30af8a
Luis Herrada <luisehk@gmail.com>
2017-02-22 22:46:03 -0600

    Cleanup settings database fied

--- fronesis/settings.py
+++ fronesis/settings.py
@@ -146,13 +146,6 @@ REST_FRAMEWORK = {
 }
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-DATABASES = {
     # heroku
     'default': dj_database_url.config(engine='django_postgrespool')
 } if os.environ.get('DATABASE_URL', '') else {
