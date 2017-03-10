01d75090805521461e04d82460d988d3cad3df05
Luis Herrada <luisehk@gmail.com>
2017-03-10 09:02:55 -0600

    UserProfile.avatar field is now optional

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class UserProfile(Profile):
-    avatar = models.ImageField()
+    avatar = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return '{} ({})'.format(self.user, self.karma)
