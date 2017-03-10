e5ce99a0468a1f164591d05a9358d01f7b32ae5c
Luis Herrada <luisehk@gmail.com>
2017-03-10 09:02:17 -0600

    Fix userprofile signal

--- users/signals.py
+++ users/signals.py
@@ -1,15 +1,18 @@
 from .models import UserProfile
+from drum.links.models import Profile
 
 
 def create_user_profile(sender, **kwargs):
-    instance = kwargs['instance']
-    created = kwargs['created']
+    u = kwargs['instance']
+    p = Profile.objects.filter(user=u)
 
-    if created:
-        UserProfile.objects.create(user=instance)
+    if p.exists():
+        p = p.first()
+        up = UserProfile.objects.filter(profile_ptr=p)
 
-
-def save_user_profile(sender, **kwargs):
-    instance = kwargs['instance']
-
-    instance.userprofile.save()
+        if not up.exists():
+            up = UserProfile(profile_ptr_id=p.pk)
+            up.__dict__.update(p.__dict__)
+            up.save()
+    else:
+        UserProfile.objects.create(user=u)
