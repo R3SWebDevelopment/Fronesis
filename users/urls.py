41f7a7f014c3ff06bfa735572f3e91db29ea3bb4
Luis Herrada <luisehk@gmail.com>
2017-03-12 22:45:10 -0600

    Add users/me endpoint

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,13 @@
-from .views import UserViewSet, UserProfileViewSet
+from .views import UserViewSet, UserProfileViewSet, MyselfView
 from rest_framework.routers import DefaultRouter
-
+from django.conf.urls import url
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'profiles', UserProfileViewSet)
-urlpatterns = router.urls
+
+urlpatterns = [
+    url(r'^me/$', MyselfView.as_view()),
+]
+
+urlpatterns += router.urls
--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from .serializers import UserSerializer, UserProfileSerializer
 from utils.mixins import OnlyAlterOwnObjectsViewSet
 from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework import viewsets
 from .models import UserProfile
 
@@ -8,6 +10,23 @@ from .models import UserProfile
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+
+class MyselfView(APIView):
+    queryset = User.objects.all()
+
+    def get_queryset(self):
+        u = self.request.user
+        q = self.queryset
+
+        return q.filter(
+            id=u.id
+        ) if u.is_authenticated else q.none()
+
+    def get(self, request, format=None):
+        return Response(
+            UserSerializer(self.get_queryset().first(), many=False).data
+        )
 
 
 class UserProfileViewSet(OnlyAlterOwnObjectsViewSet):
