eb34478ebb7bc360abaa26f135e8efc59f852b57
Ricardo Tercero Solis <ricardo.tercero.solis@gmail.com>
2017-05-02 22:35:44 -0500

    Add event templates

--- events/urls.py
+++ events/urls.py
@@ -1,7 +1,15 @@
-from .views import IndexView
+from .views import DummyView
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^index.html$', IndexView.as_view()),
+    url(r'^index.html$', DummyView.as_view(), {'template_name': 'index.html'}),
+    url(r'^list-events.html$', DummyView.as_view(), {'template_name': 'events/list-events.html',
+                                                     'body_class': 'bg-white'}),
+    url(r'^new-event.html$', DummyView.as_view(), {'template_name': 'events/new-event.html', 'body_class': 'bg-white'}),
+    url(r'^all-events.html$', DummyView.as_view(), {'template_name': 'events/all-events.html',
+                                                    'body_class': 'bg-white'}),
+    url(r'^public-view-event.html$', DummyView.as_view(), {'template_name': 'events/public-view-event.html', 'body_class': 'bg-white'}),
+
+    url(r'^booking.html$', DummyView.as_view(), {'template_name': 'booking.html', 'body_class': 'bg-white'}),
 ]
 
--- events/views.py
+++ events/views.py
@@ -1,6 +1,16 @@
 from django.views.generic import TemplateView
 
 
-class IndexView(TemplateView):
+class DummyView(TemplateView):
 
     template_name = "index.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        self.template_name = kwargs.get('template_name', "index.html")
+        self.body_class = kwargs.get('body_class', None)
+        return super(DummyView, self).dispatch(request, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(DummyView, self).get_context_data(**kwargs)
+        context['BODY_CLASS'] = self.body_class or ''
+        return context
