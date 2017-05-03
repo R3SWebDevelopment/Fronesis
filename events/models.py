a0488c0d65c79a2cad473baae7ff27d2c47f804c
ricardo.tercero@r3s.com.mx <SkipperMan8>
2017-05-02 23:05:12 -0500

    Add event model

--- events/models.py
+++ events/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import uuid
+
 
 # Create your models here.
+class Event(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4(), editable=False, null=False, blank=False)
+    name = models.CharField(null=False, blank=False, max_length=150)
+    description = models.TextField(null=False, blank=False)
+    date = models.DateField(null=False, blank=False)
+    time = models.TimeField(null=False, blank=False)
