2384d9bd5f33c9661f2f184c240d2a31380f5f9f
Ricardo Tercero Solis <ricardo.tercero.solis@gmail.com>
2017-05-21 02:17:32 -0500

    Add migration for missing field at shopping cart

--- events/tasks.py
+++ events/tasks.py
@@ -2,7 +2,7 @@
 from __future__ import absolute_import, unicode_literals
 import celery
 from utils.openpay_gateway import PaymentGateway
-from djang.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 
 @celery.task(name='events.tickets.reservation.check')
