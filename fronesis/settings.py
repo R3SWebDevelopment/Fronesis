e7edb018abf2fc35e662402de59e5ce8fd2d6b99
Ricardo Tercero Solis <ricardo.tercero.solis@gmail.com>
2017-05-23 18:17:27 -0500

    Close #7 send email notification when user is created if the user created does not have password set, generate a random one and attached into the notification email

--- fronesis/settings.py
+++ fronesis/settings.py
@@ -309,3 +309,12 @@ LOGIN_REDIRECT_URL = '/'
 ADMINS = (
     ('ricardo.tercero', 'ricardo.tercero@vordem.mx'),
 )
+
+if os.environ.get('EMAIL_HOST_USER', ''):
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'user')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'password')
+    EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+    EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '587'))
+    EMAIL_TIMEOUT = int(os.environ.get('EMAIL_TIMEOUT', '30'))
+    EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True').lower() == 'true'
+    EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL', 'False').lower() == 'true'
--- users/signals.py
+++ users/signals.py
@@ -1,9 +1,29 @@
 from versatileimagefield.image_warmer import VersatileImageFieldWarmer
+from django.db.models.signals import post_save
 from drum.links.models import Profile
 from .models import UserProfile
+from .tasks import notify_user_creation
 
 
 def create_user_profile(sender, **kwargs):
+    # When an user is created verify if has password if not generate a random one and send it.
+    # any case notify the user that he has join the plataform
+    if kwargs.get('created', False):
+        user = kwargs['instance']
+        random_password = None
+        if user.password is None or len(user.password.strip()) == 0:
+            random_password = user.__class__.objects.make_random_password()
+            # Disconnect signal to avoid transaction abortion
+            post_save.disconnect(receiver=create_user_profile, sender=sender)
+            user.set_password(random_password)
+            user.save()
+            # reconnect the signal
+            post_save.connect(create_user_profile, sender=sender)
+        notify_user_creation.apply_async(kwargs={
+            'user_id': user.id,
+            'random_password': random_password
+        })
+
     u = kwargs['instance']
     p = Profile.objects.filter(user=u)
 
