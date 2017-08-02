a8d77568e2ac18f025569f73c315ee917f602224
Ricardo Tercero Solis <ricardo.tercero@r3s.com.mx>
2017-08-02 13:14:05 -0500

    Add appointment views

--- booking/views.py
+++ booking/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import DetailView, ListView, UpdateView, CreateView, DeleteView
-from .models import Appointments, Coach
+from .models import Appointments, Coach, Client
 from crum import get_current_user
 from utils.views import FronesisBaseInnerView
 from django.core.urlresolvers import reverse
@@ -26,3 +26,68 @@ class CalendarView(ListView, FronesisBaseInnerView):
     def get_queryset(self, *args, **kwargs):
         return super(CalendarView, self).get_queryset().filter(coach=self.coach)
 
+
+class ClientsView(ListView, FronesisBaseInnerView):
+    model = Client
+    queryset = Client.objects.all()
+    template_name = 'clients.html'
+    month = None
+    year = None
+    coach = None
+
+    def dispatch(self, request, *args, **kwargs):
+        coach, created = Coach.objects.get_or_create(user=request.user)
+        self.coach = coach
+        return super(ClientsView, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(ClientsView, self).get_context_data(**kwargs)
+        context['clients'] = True
+        return context
+
+    def get_queryset(self, *args, **kwargs):
+        return super(ClientsView, self).get_queryset()
+
+
+class HistoryView(ListView, FronesisBaseInnerView):
+    model = Client
+    queryset = Client.objects.all()
+    template_name = 'clients.html'
+    month = None
+    year = None
+    coach = None
+
+    def dispatch(self, request, *args, **kwargs):
+        coach, created = Coach.objects.get_or_create(user=request.user)
+        self.coach = coach
+        return super(HistoryView, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(HistoryView, self).get_context_data(**kwargs)
+        context['history'] = True
+        return context
+
+    def get_queryset(self, *args, **kwargs):
+        return super(HistoryView, self).get_queryset()
+
+
+class BundleView(ListView, FronesisBaseInnerView):
+    model = Client
+    queryset = Client.objects.all()
+    template_name = 'clients.html'
+    month = None
+    year = None
+    coach = None
+
+    def dispatch(self, request, *args, **kwargs):
+        coach, created = Coach.objects.get_or_create(user=request.user)
+        self.coach = coach
+        return super(BundleView, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(BundleView, self).get_context_data(**kwargs)
+        context['bundles'] = True
+        return context
+
+    def get_queryset(self, *args, **kwargs):
+        return super(BundleView, self).get_queryset()
