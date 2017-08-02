91676f12b87eac749dd0eb6f01e322afd6a5aa18
Ricardo Tercero Solis <ricardo.tercero@r3s.com.mx>
2017-08-02 13:17:38 -0500

    Add appointment views

--- booking/urls.py
+++ booking/urls.py
@@ -1,7 +1,10 @@
-from .views import CalendarView
+from .views import CalendarView, HistoryView, ClientsView, BundleView
 from django.conf.urls import url
 
 urlpatterns = [
     url(r'^calendar_view/$', CalendarView.as_view(), name='calendar_view'),
+    url(r'^history/$', HistoryView.as_view(), name='history'),
+    url(r'^clients/$', ClientsView.as_view(), name='clients'),
+    url(r'^bundles/$', BundleView.as_view(), name='bundles'),
 ]
 
