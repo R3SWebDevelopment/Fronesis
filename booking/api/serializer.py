a125f31dbc40b4c71d3323e5bc072e894110e2e5
Ricardo Tercero Solis <ricardo.tercero@r3s.com.mx>
2017-08-03 14:56:32 -0500

    Remove unnecessary code

--- booking/api/serializer.py
+++ booking/api/serializer.py
@@ -1,7 +1,1 @@
 from rest_framework import serializers
-from ..models import Client
-
-class ClientSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Client
-        fields = ('id', 'coach')
