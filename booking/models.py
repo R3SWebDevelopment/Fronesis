b77a42f26ad10c40fb59a7e5d6d448d6b7760707
Ricardo Tercero Solis <ricardo.tercero@r3s.com.mx>
2017-08-03 18:46:01 -0500

    Add online_call field to appointment

--- booking/models.py
+++ booking/models.py
@@ -6,6 +6,7 @@ class Appointments(models.Model):
     coach = models.ForeignKey(Coach, null=False, related_name='appointments')
     starts_datetime = models.DateTimeField(null=True)
     ends_datetime = models.DateTimeField(null=True)
+    online_call = models.BooleanField(default=False)
     venue = models.ForeignKey(Venue, null=True, related_name='appointments')
     client = models.ForeignKey(Client, null=False, related_name='appointments')
     service = models.ForeignKey(Session, null=True, related_name='appointments')
