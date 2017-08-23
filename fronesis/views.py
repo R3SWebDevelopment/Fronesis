3f48aff4cc4e239f223a3431b1a94b59908153e1
Ricardo Tercero Solis <ricardo.tercero@r3s.com.mx>
2017-08-23 16:20:25 -0500

    Add events list

--- fronesis/views.py
+++ fronesis/views.py
@@ -1,5 +1,11 @@
 from django.views.generic import TemplateView
+from events.models import Event
 
 
 class Homepage(TemplateView):
     template_name = 'homepage.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(Homepage, self).get_context_data(*args, **kwargs)
+        context['events'] = Event.publishedEvent.all()[:3]
+        return context
