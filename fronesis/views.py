603b80de33e9cc5ad823e4d02e65c14e5f638699
Ricardo Tercero Solis <ricardo.tercero@r3s.com.mx>
2017-08-23 19:51:55 -0500

    Fix coach list slide controller on home page

--- fronesis/views.py
+++ fronesis/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView
 from events.models import Event
+from coaches.models import Coach
 
 
 class Homepage(TemplateView):
@@ -8,4 +9,9 @@ class Homepage(TemplateView):
     def get_context_data(self, *args, **kwargs):
         context = super(Homepage, self).get_context_data(*args, **kwargs)
         context['events'] = Event.publishedEvent.all()[:3]
+        context['coaches'] = Coach.objects.exclude(user__profile__userprofile__avatar='')[:3]
+        context['coaches_avatar'] = [a.get('user__profile__userprofile__avatar')
+                                     for a in Coach.objects.
+                                                  exclude(user__profile__userprofile__avatar='')
+                                                  .values('user__profile__userprofile__avatar')[:3]]
         return context
