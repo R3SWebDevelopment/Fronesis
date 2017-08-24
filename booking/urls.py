d91f5df93c3b18bbbdbf0326c436bcbf74bb29b4
Ricardo Tercero Solis <ricardo.tercero@r3s.com.mx>
2017-08-24 15:59:55 -0500

    Apply permission to booking urls

--- booking/urls.py
+++ booking/urls.py
@@ -2,15 +2,22 @@ from .views import CalendarView, HistoryView, ClientsView, BundleView, AddAppoin
     AppointmentRequestRemoveView, AppointmentRequestConfirmView, AppointmentClientSideModalView
 from django.conf.urls import url
 
+from django.contrib.auth.decorators import permission_required
+
 urlpatterns = [
     url(r'^calendar_view/$', CalendarView.as_view(), name='calendar_view'),
-    url(r'^history/$', HistoryView.as_view(), name='history'),
-    url(r'^clients/$', ClientsView.as_view(), name='clients'),
-    url(r'^bundles/$', BundleView.as_view(), name='bundles'),
-    url(r'^add/$', AddAppointmentView.as_view(), name='add_appointment'),
-    url(r'^confirmation/$', AppointmentRequestView.as_view(), name='confirmation'),
-    url(r'^confirmation/(?P<pk>\d+)/remove/$', AppointmentRequestRemoveView.as_view(), name='confirmation_remove'),
-    url(r'^confirmation/(?P<pk>\d+)/confirm/$', AppointmentRequestConfirmView.as_view(), name='confirmation_confirm'),
+    url(r'^history/$', permission_required('booking.appointment_history')(HistoryView.as_view()), name='history'),
+    url(r'^clients/$', permission_required('booking.view_clients')(ClientsView.as_view()), name='clients'),
+    url(r'^bundles/$', permission_required('booking.view_active_bundles')(BundleView.as_view()), name='bundles'),
+    url(r'^add/$', permission_required('booking.add_appointment_coach_side')(AddAppointmentView.as_view()), name='add_appointment'),
+    url(r'^confirmation/$', permission_required('booking.confirm_appointment')(AppointmentRequestView.as_view()),
+        name='confirmation'),
+    url(r'^confirmation/(?P<pk>\d+)/remove/$',
+        permission_required('booking.confirm_appointment')(AppointmentRequestRemoveView.as_view()),
+        name='confirmation_remove'),
+    url(r'^confirmation/(?P<pk>\d+)/confirm/$',
+        permission_required('booking.confirm_appointment')(AppointmentRequestConfirmView.as_view()),
+        name='confirmation_confirm'),
     url(r'^client-side/(?P<pk>\d+)/modal/$', AppointmentClientSideModalView.as_view(), name='client_side_modal'),
 ]
 
