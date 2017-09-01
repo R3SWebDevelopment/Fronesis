9b0a051af118b4b0956507b2cff40fe8d30effce
Ricardo Tercero Solis <ricardo.tercero@r3s.com.mx>
2017-08-31 23:13:24 -0500

    Fix user profile serializer

--- users/serializers.py
+++ users/serializers.py
@@ -6,13 +6,33 @@ from .models import UserProfile
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
-    avatar = VersatileImageFieldSerializer(
-        sizes='userprofile_avatar'
-    )
+    # avatar = VersatileImageFieldSerializer(
+    #     sizes='userprofile_avatar'
+    # )
+    avatar = serializers.SerializerMethodField()
 
     class Meta:
         model = UserProfile
         fields = ['bio', 'avatar']
+
+
+    def get_avatar(self, obj):
+        avatar = obj.avatar
+        if obj.avatar:
+            return{
+                'full_size': obj.avatar.url,
+                'medium_square_crop': obj.avatar.crop['280x280'].url,
+                'small_square_crop': obj.avatar.crop['50x50'].url,
+                'thumbnail': obj.avatar.crop['280x280'].url,
+            }
+        else:
+            return{
+                'full_size': 'http://eadb.org/wp-content/uploads/2015/08/profile-placeholder.jpg',
+                'medium_square_crop': 'http://eadb.org/wp-content/uploads/2015/08/profile-placeholder.jpg',
+                'small_square_crop': 'http://eadb.org/wp-content/uploads/2015/08/profile-placeholder.jpg',
+                'thumbnail': 'http://eadb.org/wp-content/uploads/2015/08/profile-placeholder.jpg',
+            }
+
 
 
 class ProfileSerializer(serializers.ModelSerializer):
